ebfcf8b64dc276fbe97dfc214d8ead5d795545e7
karadiakhil <karadiakhil@gmail.com>
2021-04-20 16:19:01 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "devops.cqmdt0h7v6tb.eu-west-1.rds.amazonaws.com";
+$username = "admin";
+$password = "admin123";
 $db = "onlineshop";
 
 // Create connection
